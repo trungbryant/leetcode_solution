013fe0e817260cfbcffcbfa7ca6ca8a8f8ba0863
TrungBui <trung.bui1@tyme.com>
2022-06-04 23:23:07 +0700

    resolve problem 04

--- src/Solution.java
+++ src/Solution.java
@@ -89,4 +89,49 @@ class Solution{
         }
         return Math.max(longest, set.size());
     }
+
+    /**
+     * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+     * The overall run time complexity should be O(log (m+n)))
+     * 
+     * @param nums1
+     * @param nums2
+     * @return the median of the two sorted arrays
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        double median = 0.0;
+        int min = 0;
+        double avg = (double) (nums1.length + nums2.length)/2;
+        while(k <= (nums1.length + nums2.length)/2){
+            if(i < nums1.length && j < nums2.length){
+                if(nums1[i] < nums2[j]){
+                    min = nums1[i];
+                    i++;
+                }else{
+                    min = nums2[j];
+                    j++;
+                }
+            }else if(i < nums1.length ){
+                min = nums1[i];
+                i++;
+            }else{
+                min = nums2[j];
+                j++;
+            }
+            if(k == (int) avg - 1 && avg == (int) avg){
+                median += min;
+            }
+            else if(k == (int)avg){
+                median += min;
+                if(avg == (int) avg){
+                    median /= 2;
+                }
+            }
+            k++;
+        }
+        return median;
+    }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -1,16 +1,18 @@
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class SolutionTest {
+    Solution solution = new Solution();
     @Test
     public void testTwoSumCase01(){
         int[] nums = new int[]{2,7,11,15};
         int target = 9;
         int[] expected = new int[]{0,1};
-        assertArrayEquals(expected ,new Solution().twoSum(nums, target));
+        assertArrayEquals(expected ,solution.twoSum(nums, target));
     }
 
     @Test
@@ -18,7 +20,7 @@ public class SolutionTest {
         int[] nums = new int[]{3,2,4};
         int target = 6;
         int[] expected = new int[]{1,2};
-        assertArrayEquals(expected ,new Solution().twoSum(nums, target));
+        assertArrayEquals(expected ,solution.twoSum(nums, target));
     }
 
     @Test
@@ -26,7 +28,7 @@ public class SolutionTest {
         int[] nums = new int[]{3,3};
         int target = 6;
         int[] expected = new int[]{0,1};
-        assertArrayEquals(expected ,new Solution().twoSum(nums, target));
+        assertArrayEquals(expected ,solution.twoSum(nums, target));
     }
 
     @Test
@@ -34,7 +36,7 @@ public class SolutionTest {
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
         ListNode expected = new ListNode(7, new ListNode(0, new ListNode(8)));
-        ListNode actual = new Solution().addTwoNumbers(l1, l2);
+        ListNode actual = solution.addTwoNumbers(l1, l2);
         assertNotNull(actual);
         while(expected != null && actual != null ){
             assertEquals(expected.val, actual.val);
@@ -48,7 +50,7 @@ public class SolutionTest {
         ListNode l1 = new ListNode(0);
         ListNode l2 = new ListNode(0);
         ListNode expected = new ListNode(0);
-        ListNode actual = new Solution().addTwoNumbers(l1, l2);
+        ListNode actual = solution.addTwoNumbers(l1, l2);
         assertNotNull(actual);
         while(expected != null && actual != null ){
             assertEquals(expected.val, actual.val);
@@ -62,7 +64,7 @@ public class SolutionTest {
         ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
         ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
         ListNode expected = new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))));
-        ListNode actual = new Solution().addTwoNumbers(l1, l2);
+        ListNode actual = solution.addTwoNumbers(l1, l2);
         assertNotNull(actual);
         while(expected != null && actual != null ){
             assertEquals(expected.val, actual.val);
@@ -76,7 +78,7 @@ public class SolutionTest {
         ListNode l1 = new ListNode(1, new ListNode(8));
         ListNode l2 = new ListNode(0);
         ListNode expected = new ListNode(1, new ListNode(8));
-        ListNode actual = new Solution().addTwoNumbers(l1, l2);
+        ListNode actual = solution.addTwoNumbers(l1, l2);
         assertNotNull(actual);
         while(expected != null && actual != null ){
             assertEquals(expected.val, actual.val);
@@ -89,34 +91,58 @@ public class SolutionTest {
     public void testLengthOfLongestSubstringCase01(){
         String s = "abcabcbb";
         int expected = 3;
-        assertEquals(expected, new Solution().lengthOfLongestSubString(s));
+        assertEquals(expected, solution.lengthOfLongestSubString(s));
     }
 
     @Test
     public void testLengthOfLongestSubstringCase02(){
         String s = "bbbbb";
         int expected = 1;
-        assertEquals(expected, new Solution().lengthOfLongestSubString(s));
+        assertEquals(expected, solution.lengthOfLongestSubString(s));
     }
 
     @Test
     public void testLengthOfLongestSubstringCase03(){
         String s = "pwwkew";
         int expected = 3;
-        assertEquals(expected, new Solution().lengthOfLongestSubString(s));
+        assertEquals(expected, solution.lengthOfLongestSubString(s));
     }
 
     @Test
     public void testLengthOfLongestSubstringCase04(){
         String s = " ";
         int expected = 1;
-        assertEquals(expected, new Solution().lengthOfLongestSubString(s));
+        assertEquals(expected, solution.lengthOfLongestSubString(s));
     }
 
     @Test
     public void testLengthOfLongestSubstringCase05(){
         String s = "au";
         int expected = 2;
-        assertEquals(expected, new Solution().lengthOfLongestSubString(s));
+        assertEquals(expected, solution.lengthOfLongestSubString(s));
+    }
+
+    @Test
+    public void testFindMedianSortedArraysCase01(){
+        int[] nums1 = new int[]{1,3};
+        int[] nums2 = new int[]{2};
+        double expected = 2.00000;
+        assertTrue(expected == solution.findMedianSortedArrays(nums1, nums2));
+    }
+
+    @Test
+    public void testFindMedianSortedArraysCase02(){
+        int[] nums1 = new int[]{1,2};
+        int[] nums2 = new int[]{3,4};
+        double expected = 2.50000;
+        assertTrue(expected == solution.findMedianSortedArrays(nums1, nums2));
+    }
+
+    @Test
+    public void testFindMedianSortedArraysCase03(){
+        int[] nums1 = new int[]{};
+        int[] nums2 = new int[]{2,3};
+        double expected = 2.50000;
+        assertTrue(expected == solution.findMedianSortedArrays(nums1, nums2));
     }
 }
